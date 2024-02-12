44ac617e0e0ea9b4c5e058410ff8f5ade0bbff87
benj-yalung <benjamin.yalung2020@gmail.com>
2024-02-12 17:45:47 +0800

    Add plugin notice and add initial method in functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
@@ -22,3 +24,42 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+// Check if CPT UI is active
+if ( ! is_plugin_active( 'custom-post-type-ui/custom-post-type-ui.php' ) ) {
+    // The CPT UI plugin is not active, add an admin notice
+    add_action( 'admin_notices', 'cptui_plugin_not_active_notice' );
+}
+
+// Check if ACF is active
+if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) ) {
+    // The ACF plugin is not active, add an admin notice
+    add_action( 'admin_notices', 'acf_plugin_not_active_notice' );
+}
+
+// Admin notice for CPT UI plugin
+function cptui_plugin_not_active_notice() {
+    ?>
+    <div class="notice notice-warning is-dismissible">
+        <p><?php _e( 'The CPT UI plugin is not active. Please activate it to use all features of this theme.', 'text-domain' ); ?></p>
+    </div>
+    <?php
+}
+
+// Admin notice for ACF plugin
+function acf_plugin_not_active_notice() {
+    ?>
+    <div class="notice notice-warning is-dismissible">
+        <p><?php _e( 'The ACF plugin is not active. Please activate it to use all features of this theme.', 'text-domain' ); ?></p>
+    </div>
+    <?php
+}
+
+if ( ! function_exists('listPortfolio') ) {
+    /**
+     * List the cpt "portfolio"
+     */
+    function listPortfolio() {
+        // TODO: Get portfolio post types
+    }
+}
