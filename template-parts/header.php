e1464d69dc3894b40040d00b219081ba1f0d3c10
benj-yalung <benjamin.yalung2020@gmail.com>
2024-02-13 18:18:27 +0800

    Add header and hero section content

--- template-parts/header.php
+++ template-parts/header.php
@@ -12,35 +12,37 @@ $site_name = get_bloginfo( 'name' );
 $tagline   = get_bloginfo( 'description', 'display' );
 ?>
 
-<header id="site-header" class="site-header">
-
-	<div class="site-branding">
-		<?php
-		if ( has_custom_logo() ) {
-			the_custom_logo();
-		} elseif ( $site_name ) {
-			?>
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr__( 'Home', 'hello-elementor' ); ?>" rel="home">
-					<?php echo esc_html( $site_name ); ?>
-				</a>
-			</h1>
-			<p class="site-description">
-				<?php
-				if ( $tagline ) {
-					echo esc_html( $tagline );
-				}
+<div class="header__container">
+	<header id="site-header" class="site-header">
+	
+		<div class="site-branding">
+			<?php
+			if ( has_custom_logo() ) {
+				the_custom_logo();
+			} elseif ( $site_name ) {
 				?>
-			</p>
-		<?php } ?>
-	</div>
-
-	<nav class="site-navigation">
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="/portfolio-list">Portfolio</a></li>
-			<li><a href="#!"><?php echo prepare_weather_data(true); ?></a></li>
-		</ul>
-	</nav>
-</header>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr__( 'Home', 'hello-elementor' ); ?>" rel="home">
+						<?php echo esc_html( $site_name ); ?>
+					</a>
+				</h1>
+				<p class="site-description">
+					<?php
+					if ( $tagline ) {
+						echo esc_html( $tagline );
+					}
+					?>
+				</p>
+			<?php } ?>
+		</div>
+	
+		<nav class="site-navigation">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="/portfolio-list">Portfolio</a></li>
+				<li><a href="#!"><?php echo prepare_weather_data(true); ?></a></li>
+			</ul>
+		</nav>
+	</header>
+</div>
--- template-parts/weather-data.php
+++ template-parts/weather-data.php
@@ -5,5 +5,5 @@ $data = data_get($args, 'data', []);
 <div class="hero__weather">
     <p><?php echo data_get($data, 'windLabel'); ?></p>
     <p><?php echo data_get($data, 'temperatureLabel'); ?></p>
-    <p>As of: <?php echo date('y-m-d h-i-s'); ?></p>
+    <p>As of <?php echo date('F j, Y h:i A'); ?></p>
 </div>
